7d0d1a5d49e255e34d051b6bbf0f083dd07466bf
Patrick Bourke <pbourke@gmail.com>
2018-08-26 00:08:06 -0700

    Added emacs.d/init.el to replace .emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,31 @@
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://stable.melpa.org/packages/") t)
+;(add-to-list 'package-archives
+;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(package-initialize)
+
+;(package-refresh-contents)
+
+(setq auto-install-packages
+      '(solarized-theme))
+
+(dolist (pkg auto-install-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+(load-theme 'solarized-dark t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (solarized-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- linkify.sh
+++ linkify.sh
@@ -7,4 +7,4 @@ DIR=`dirname "$0"`
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/vim ~/.vim
 ln -s $DIR/gitconfig ~/.gitconfig
-ln -s $DIR/emacs ~/.emacs
+ln -s $DIR/emacs.d ~/.emacs.d
